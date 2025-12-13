3a8da333b99dd72366e34e63c41a1e60260957d3
Haneena <haneenahani279@gmail.com>
2025-12-13 12:02:54 +0530

    modified project

--- data.py
+++ data.py
@@ -8,3 +8,18 @@ def add(a,b):
 
 def sub(a,b):
     return a-b
+
+def mul(a,b):
+    return a*b
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+
+    
